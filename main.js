96753c32a350c8bc03ec4f2616178cb3a1c42d6f
Putipong Sailen <tyice789@gmail.com>
2023-06-29 14:46:17 +0700

    4.5, 4.6 and 4.7

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ createApp({
         const product = ref('Boots')
         const image = ref('./assets/images/socks_green.jpg')
         const inStock = ref(true)
+        const inventory = ref(11)
         const link = ref('https://www.camt.cmu.ac.th')
         return {
             product,
             image,
             inStock,
+            inventory,
             link
         }
     }
