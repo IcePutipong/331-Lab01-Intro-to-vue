f61bdbf3afa0de82fd99b7bd0ebfb92b86831ec5
Putipong Sailen <tyice789@gmail.com>
2023-06-30 02:12:12 +0700

    5.1 and 5.2

--- main.js
+++ main.js
@@ -5,14 +5,20 @@ createApp({
         const product = ref('Boots')
         const image = ref('./assets/images/socks_green.jpg')
         const inStock = ref(true)
-        const onSale = ref(true)
+        const onSale = ref(false)
         const inventory = ref(100)
+        const details = ref([
+            '50% cotton',
+            '30% wool',
+            '20% polyester'
+        ])
         const link = ref('https://www.camt.cmu.ac.th')
         return {
             product,
             image,
             inStock,
             inventory,
+            details,
             onSale,
             link
         }
